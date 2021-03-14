2a57833a9b8ebb4e680b899f03ef15805300c52a
MercybirungiS <mercyssbirungi@gmail.com>
2021-03-14 11:42:46 +0300

    Assignment3

--- src/Assignment3.kt
+++ src/Assignment3.kt
@@ -1,8 +1,10 @@
 fun main(){
-    school()
-    println(identity("Hope",15) )
-    println(name("mercy") )
-    whoIam("Mercy")
+   school()
+    println(identity("Faith",15) )
+    println(name("Christine") )
+    (whoIam("Hope") )
+    (whoIam("Mercy") )
+
 }
 fun school(){
     var school="akirachix"
@@ -15,18 +17,15 @@ fun identity(x:String ,y:Int ):String {
     return "Hi, my name is $x and i am $y years old "
 }
 fun name(word:String):Int {
-    var myName=word.length
-    return myName
+    return    word.length
+
 }
 fun whoIam(name:String){
-    var name="That's me"
-    if (name==name ){
+    if (name=="Mercy"){
         println("That's me!")
     }
     else{
         println("i do not know who that is ")
     }
-
-
-
 }
+
